d62dab8bec64ec7e15a6c1d2a9aefa797304d225
ajsarmah <ajsarmah@tutanota.com>
2022-10-09 07:52:58 +0530

    Initial commit

---
+++ qtile/config.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+import socket
+import subprocess
+from libqtile import qtile
+from libqtile.config import Click, Drag, Group, KeyChord, Key, Match, Screen
+from libqtile.command import lazy
+from libqtile import layout, bar, widget, hook
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
+from typing import List  # noqa: F401from typing import List  # noqa: F401
+from utils import battery_display, connect
+
+class Colors:
+    BLACK: str = "000000"
+    DARK: str = "1A1B26"
+    WHITE: str = "ffffff"
+    PRIMARY: str = "0074e4"
+    SECONDARY: str = "3D59A1"
+
+mod = "mod4"              # Sets mod key to SUPER/WINDOWS
+terminal = "alacritty"      # My terminal of choice
+myBrowser = "firefox" # My browser of choice
+palette = Colors()
+
+keys = [
+    # A list of available commands that can be bound to keys can be found
+    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
+    # Switch between windows
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.grow(),lazy.layout.increase_nmaster(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.grow(),lazy.layout.decrease_nmaster(), desc="Move focus up"),
+    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    # Move windows between left/right columns or move up/down in current stack.
+    # Moving out of range in Columns layout will create new column.
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    # Grow windows. If current window is on the edge of screen and direction
+    # will be to screen edge - window would shrink.
+    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with
+    # multiple stack panes
+    Key(
+        [mod, "shift"],
+        "Return",
+        lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack",
+    ),
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    # Toggle between different layouts as defined below
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+]
+
+layout_theme = {
+                "border_width": 2,
+                "margin": 8,
+                "border_focus": "e1acff",
+                "border_normal": "1D2330"
+                }
+
+layouts = [
+    #layout.MonadWide(**layout_theme),
+    #layout.Bsp(**layout_theme),
+    #layout.Stack(stacks=2, **layout_theme),
+    layout.Columns(**layout_theme),
+    #layout.RatioTile(**layout_theme),
+    #layout.Tile(shift_windows=True, **layout_theme),
+    #layout.VerticalTile(**layout_theme),
+    #layout.Matrix(**layout_theme),
+    #layout.Zoomy(**layout_theme),
+    #layout.MonadTall(**layout_theme),
+    layout.Max(**layout_theme),
+    #layout.Stack(num_stacks=2),
+    #layout.RatioTile(**layout_theme),
+    #layout.Floating(**layout_theme)
+]
+
+groups = [Group(i) for i in "123456789"]
+
+for i in groups:
+    keys.extend(
+        [
+            # mod1 + letter of group = switch to group
+            Key(
+                [mod],
+                i.name,
+                lazy.group[i.name].toscreen(),
+                desc="Switch to group {}".format(i.name),
+            ),
+            # mod1 + shift + letter of group = switch to & move focused window to group
+            Key(
+                [mod, "shift"],
+                i.name,
+                lazy.window.togroup(i.name, switch_group=True),
+                desc="Switch to & move focused window to group {}".format(i.name),
+            ),
+            # Or, use below if you prefer not to switch to that group.
+            # # mod1 + shift + letter of group = move focused window to group
+            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+            #     desc="move focused window to group {}".format(i.name)),
+        ]
+    )
+
+widget_defaults = dict(
+    font='Fira Code',
+    fontsize=13,
+    padding=7,
+)
+extension_defaults = widget_defaults.copy()
+# Separator Widget
+# separator = widget.Sep(foreground=palette.DARK)
+separator = widget.TextBox(text=" ",padding=5)
+
+screens = [
+    Screen(
+        top=bar.Bar(
+            [
+                widget.GroupBox(
+                    fontsize=13,
+                    padding_x=5,
+                    borderwidth=0,
+                    active=palette.WHITE,
+                    inactive=palette.WHITE,
+                    rounded=True,
+                    font="Fira Code",
+                    highlight_method="block",
+                    highlight_color=palette.DARK,
+                    block_highlight_text_color=palette.WHITE,
+                    this_current_screen_border=palette.SECONDARY,
+                    foreground=palette.DARK,
+                    background=palette.DARK,
+                    hide_unused=True
+                ),
+                separator,
+                widget.Prompt(),
+                #widget.WindowName(fontsize=15, format='{name}'),
+                widget.Spacer(),
+                widget.Systray(),
+                separator,
+                widget.Battery(format=battery_display()),
+                separator,
+                widget.TextBox(text=connect(),fontsize=20,padding=0),
+                widget.Wlan(format='  {essid}',padding=0), # The Wlan widget uses a library called iwlib as dependency so to use it we must install 'iwlib' via the pip package manager.
+                separator,
+                separator,
+                widget.TextBox(text="", fontsize = 20, padding = 0),
+                widget.Clock(format='  %I:%M %p', padding=0),
+                separator,
+                separator,
+                widget.TextBox(text="", fontsize=20 ,padding=0),
+                widget.Clock(format = '  %a %d/%m/%y',padding=0),
+                separator,
+            ],
+            20,
+            margin=[10, 10, 0, 10],
+            background=palette.DARK,
+            opacity=1,
+        ),
+    ),
+]
+
+@hook.subscribe.startup
+def autostart():
+    home = os.path.expanduser('~')
+    subprocess.run([home + '/.config/qtile/autostart.sh'])
+
---
+++ qtile/utils.py
@@ -0,0 +1,35 @@
+import psutil
+import urllib.request
+
+def get_battery():
+    battery = psutil.sensors_battery()
+    percent = battery.percent
+    if battery.power_plugged:
+        if percent < 50:
+            return " "
+        if percent >= 50 and percent < 75:
+            return " "
+        if percent >= 75 and percent < 100:
+            return " "
+        if percent == 100:
+            return " "
+    else:
+        if percent < 50:
+            return ""
+        if percent >= 50 and percent < 75:
+            return ""
+        if percent >= 75 and percent < 100:
+            return ""
+        if percent == 100:
+            return ""
+
+def connect(host='http://google.com'):
+    try:
+        urllib.request.urlopen(host)
+        return "直"
+    except:
+        return "睊"
+
+def battery_display():
+    battery = get_battery()
+    return battery + '  {percent:2.0%}'
